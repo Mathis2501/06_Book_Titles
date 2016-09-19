90723ea82386b7a0b8e77b05f0666b381dbda6df
Mathias Bellerby <mathis2501@gmail.com>
2016-09-19 09:21:56 +0200

    All Tests Pass

---
+++ 06_Book_Titles/book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Book_Titles
+{
+    
+    class Book
+    {
+
+
+        private string title;
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                string[] n = value.Split(' ');
+                string NewString = "";
+                char[] a = value.ToLower().ToCharArray();
+                bool first = true;
+                string[] SkipList = { "a", "an", "and", "over", "the", "in", "of" };
+
+
+                foreach (string element in n)
+                {
+                    if (!first)
+                        NewString += " ";
+
+                    if (first || !SkipList.Contains(element))
+                    {
+                        NewString += char.ToUpper(element[0]) + element.Substring(1);
+                    }
+
+
+                    else
+                    {
+                        NewString += element;
+                    }
+
+                    first = false;
+                    
+
+                }
+                this.title = NewString;
+            }
+        }
+    }
+}
+
